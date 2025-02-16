ebefe0449fbb915e1f2c14e8ac5142c0ce7bd886
Naeem <inaeem@protonmail.com>
2025-02-16 22:23:13 +0500

    :recycle: refactor the server code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  return res.json({ content: 'We are up and running. 🚀' });
 });
 
 module.exports = router;
