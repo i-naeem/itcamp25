865ccb26daa2619bb872991ef8f2a7e7bbc0b38d
Naeem <inaeem@protonmail.com>
2025-02-17 14:20:19 +0500

    :saprkles: added feature to cast vote

--- server/routes/jokes.route.js
+++ server/routes/jokes.route.js
@@ -15,4 +15,52 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const joke =  await Joke.findById(id);
+    if (!joke) {
+      return res.status(404).json({ message: 'No jokes found' });
+    }
+    return res.json(joke);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/:id', async (req, res, next) => {
+  const { action, value } = req.body;
+  const { id } = req.params;
+
+  try {
+    const joke = await Joke.findById(id);
+    if (!joke) {
+      return res.status(404).json({ message: 'No jokes found' });
+    }
+
+    if (action !== 'increment' && action !== 'decrement') {
+      return res.status(400).json({ message: 'Invalid action. Use "increment" or "decrement".' });
+    }
+
+    const vote = joke.votes.find((vote) => vote.label === value);
+    if (!vote) {
+      return res.status(400).json({ message: `Invalid vote label: ${value}` });
+    }
+
+    if (action === 'increment') {
+      vote.value += 1;
+    } else if (action === 'decrement') {
+      vote.value -= 1;
+    }
+
+    await joke.save();
+
+    return res.json(joke); 
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 module.exports = router;
